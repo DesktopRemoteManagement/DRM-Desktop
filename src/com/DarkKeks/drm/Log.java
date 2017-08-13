15f0892dc4c991a121878985148872e711eb31cc
DarkKeks <darkkeks@rambler.ru>
2017-08-14 01:11:09 +0300

    Changed Log formatter

--- src/com/DarkKeks/drm/Log.java
+++ src/com/DarkKeks/drm/Log.java
@@ -1,10 +1,20 @@
 package com.DarkKeks.drm;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Date;
+import java.util.logging.*;
 
 public class Log {
     private static Logger logger = Logger.getLogger(Log.class.getName());
+
+    static {
+        logger.setUseParentHandlers(false);
+
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setFormatter(new LogFormatter());
+        logger.addHandler(handler);
+    }
 
     public static void info(String line){
         logger.info(line);
@@ -16,5 +26,24 @@ public class Log {
 
     public static void logException(Throwable th){
         logger.log(Level.WARNING, "Exception: ", th);
+    }
+
+    private static class LogFormatter extends Formatter {
+
+        @Override
+        public String format(LogRecord record) {
+            String message = formatMessage(record);
+            Throwable th = record.getThrown();
+            if(th != null) {
+                StringWriter sw = new StringWriter();
+                th.printStackTrace(new PrintWriter(sw));
+                message += sw.toString();
+            }
+            return String.format(
+                    "%1$s [%2$s] %3$s\n",
+                    new Date(record.getMillis()).toString(),
+                    record.getLevel().getName(),
+                    message);
+        }
     }
 }
